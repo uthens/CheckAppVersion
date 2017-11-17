4aab38466fb477c3a5f7e66b275e1cb31f6115d3
Uthen <uthen@mycostech.com>
2017-11-17 16:05:13 +0700

    bug fixes

--- CheckAppVersion/ViewController.h
+++ CheckAppVersion/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIButton *btnCheckAppVersion;
+@property (strong, nonatomic) IBOutlet UILabel *lblShowMoreInfo;
 
 @end
 
--- CheckAppVersion/ViewController.m
+++ CheckAppVersion/ViewController.m
@@ -17,6 +17,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    _lblShowMoreInfo.hidden = YES;
 }
 
 
@@ -25,5 +26,59 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnCheckAppVersion:(id)sender {
+    [self.btnCheckAppVersion setTitle:@"Checking..."
+                             forState:UIControlStateNormal];
+    [self needsUpdate];
+}
+
+-(BOOL) needsUpdate{
+    NSDictionary* infoDictionary = [[NSBundle mainBundle] infoDictionary];
+    
+    //NSString* appID = infoDictionary[@"CFBundleIdentifier"];
+    //Sample app bundle
+    
+    NSString* appID = @"com.clubprophetsystems.cpsair";
+    NSString* appURL = @"http://itunes.apple.com/lookup?bundleId=%@";
+    NSString* appMassage = @"Need to update App store \n V:%@ \n But Your device \n V:%@";
+    NSString* appMessage1 = @"Same version or \n on local device to higher than app store version.";
+    
+    NSURL* url = [NSURL URLWithString:[NSString stringWithFormat:appURL, appID]];
+    NSData* data = [NSData dataWithContentsOfURL:url];
+    NSDictionary* lookup = [NSJSONSerialization JSONObjectWithData:data
+                                                           options:0
+                                                             error:nil];
+    
+    if ([lookup[@"resultCount"] integerValue] == 1){
+        NSString* appStoreVersion = lookup[@"results"][0][@"version"];
+        NSString* currentVersion = infoDictionary[@"CFBundleShortVersionString"];
+        if (![appStoreVersion isEqualToString:currentVersion]){
+            NSLog(@"Need to update [%@ != %@]", appStoreVersion, currentVersion);
+            
+            [self.btnCheckAppVersion setTitle:@"Need to update"
+                                     forState:UIControlStateNormal];
+    
+            NSString* sMessage = [NSString stringWithFormat:
+                                  appMassage,
+                                  appStoreVersion,
+                                  currentVersion];
+            _lblShowMoreInfo.textAlignment = NSTextAlignmentCenter;
+            _lblShowMoreInfo.hidden = NO;
+            _lblShowMoreInfo.text = sMessage;
+            _lblShowMoreInfo.numberOfLines = 0;
+            
+            return YES;
+        }else{
+        // Same version or on local device to higher than app store version.
+        [self.btnCheckAppVersion setTitle:@"Try again..."
+                                 forState:UIControlStateNormal];
+            _lblShowMoreInfo.text = appMessage1;
+            _lblShowMoreInfo.textAlignment = NSTextAlignmentCenter;
+            _lblShowMoreInfo.hidden = NO;
+            _lblShowMoreInfo.numberOfLines = 0;
+        }
+    }
+    return NO;
+}
 
 @end
